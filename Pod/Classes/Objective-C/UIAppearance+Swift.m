7967dc938e37f0292c1c78ccf2e9bdb0eb5f84b1
Ruslan Timchenko <21226333+Semty@users.noreply.github.com>
2023-05-28 22:31:43 +0200

    fix: Some small warnings

--- Pod/Classes/Objective-C/UIAppearance+Swift.m
+++ Pod/Classes/Objective-C/UIAppearance+Swift.m
@@ -15,18 +15,19 @@
     NSUInteger count = containers.count;
     NSAssert(count <= 10, @"The count of containers greater than 10 is not supported.");
     
-    return [self appearanceWhenContainedIn:
-            count > 0 ? containers[0] : nil,
-            count > 1 ? containers[1] : nil,
-            count > 2 ? containers[2] : nil,
-            count > 3 ? containers[3] : nil,
-            count > 4 ? containers[4] : nil,
-            count > 5 ? containers[5] : nil,
-            count > 6 ? containers[6] : nil,
-            count > 7 ? containers[7] : nil,
-            count > 8 ? containers[8] : nil,
-            count > 9 ? containers[9] : nil,
-            nil];
+    NSArray *appearanceContainers = @[
+        count > 0 ? containers[0] : [NSNull null],
+        count > 1 ? containers[1] : [NSNull null],
+        count > 2 ? containers[2] : [NSNull null],
+        count > 3 ? containers[3] : [NSNull null],
+        count > 4 ? containers[4] : [NSNull null],
+        count > 5 ? containers[5] : [NSNull null],
+        count > 6 ? containers[6] : [NSNull null],
+        count > 7 ? containers[7] : [NSNull null],
+        count > 8 ? containers[8] : [NSNull null],
+        count > 9 ? containers[9] : [NSNull null]
+    ];
+    return [self appearanceWhenContainedInInstancesOfClasses:appearanceContainers];
 }
 
 @end
