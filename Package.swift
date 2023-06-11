5fb292406d5c8cfe323f5d4912d85901c5ee362c
Ruslan Timchenko <21226333+Semty@users.noreply.github.com>
2023-06-11 15:22:20 +0200

    feat: Added support for a Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Chameleon",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "Chameleon",
+            targets: ["Chameleon"]),
+        .library(
+            name: "ChameleonSwift",
+            targets: ["ChameleonSwift"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Chameleon",
+            publicHeadersPath: "include"
+        ),
+        .target(
+            name: "ChameleonSwift",
+            dependencies: ["Chameleon"]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
